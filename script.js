ce717ea51b0d30a8b5541418342d209d98670d87
Debora Ribeiro <debii.ribeiro@gmail.com>
2025-04-27 16:09:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function mostrarDoacao() {
+  document.getElementById('conteudo').style.display = 'none';
+  document.getElementById('impacto').style.display = 'none';
+  document.getElementById('doacao').style.display = 'block';
+}
+
+
+function mostrarImpacto() {
+  document.getElementById('conteudo').style.display = 'none';
+  document.getElementById('doacao').style.display = 'none';
+  document.getElementById('impacto').style.display = 'block';
+}
