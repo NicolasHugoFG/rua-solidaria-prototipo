c54dd40b1e44abb237bb9871e45dea2efb159d90
Nicolas-Hugo Ferreira Ouellet <13525119932@ulife.com.br>
2025-06-02 04:40:20 -0300

    oii deboraa

---
+++ README.md
@@ -0,0 +1,39 @@
+🛣️ Rua Solidária
+Um projeto de site voltado à arrecadação de doações para pessoas em situação de vulnerabilidade social. Com design simples, mensagem humanitária e registro de impacto social, o sistema permite que qualquer pessoa doe com facilidade.
+
+🌟 Funcionalidades
+Página inicial com mensagem de impacto social
+Animações suaves e layout responsivo
+Botão para realizar doações via PIX (com QR Code)
+Formulário para registrar o valor doado
+Seção de impacto: número de pessoas beneficiadas, marmitas doadas, valor arrecadado
+Integração com backend PHP (localhost) para salvar registros
+🖥️ Tecnologias Utilizadas
+HTML5 — estrutura da página
+CSS3 — estilo com gradientes e animações
+JavaScript — controle da navegação e envio de dados
+PHP — registro dos dados da doação (localmente)
+XAMPP — servidor local para testes
+📁 Estrutura de Arquivos
+📦 rua_solidaria ┣ 📄 index.html ┣ 📄 style.css ┣ 📄 script.js ┣ 📄 registrar_doacao.php ┣ 📄 imagens (como QR Code e logo) ┗ 📄 README.md
+
+🚀 Como Executar Localmente
+Clone o repositório:
+
+git clone https://github.com/Debyrs2/Prototipo-rua-solidaria.git
+Coloque uma pasta no diretório htdocsdo XAMPP:
+
+http://localhost/rua_solidaria
+
+Início do XAMPP (Apache + MySQL)
+
+Acesse no navegador: http://localhost/rua_solidaria
+
+Faça uma simulação de ação para ver o funcionamento
+
+📬 Contato Caso deseje contribuir com o projeto ou sugestão de melhorias, entre em contato:
+
+📧 debii.ribeiro@gmail.com
+--------- BUILD -----------
+#db running at port 3308
+npx tsx server.ts
---
+++ create_db.sql
@@ -0,0 +1,104 @@
+CREATE TABLE IF NOT EXISTS userType(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS accountType(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS profile(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    description TEXT NOT NULL,
+    picture VARCHAR(200),
+    email_publico VARCHAR(100)
+);
+
+CREATE TABLE IF NOT EXISTS account(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    tipo INT NOT NULL,
+    id_outro INT NOT NULL,
+    FOREIGN KEY (tipo) REFERENCES accountType(id)
+);
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    show_donations TINYINT(1) DEFAULT 1,
+    tipo INT NOT NULL,
+    profile_id INT NOT NULL,
+    FOREIGN KEY (profile_id) REFERENCES profile(id)
+);
+
+CREATE TABLE IF NOT EXISTS instituicoes(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    cnpj VARCHAR(100) NOT NULL UNIQUE,
+    link VARCHAR(100), 
+    profile_id INT NOT NULL,
+    FOREIGN KEY (profile_id) REFERENCES profile(id)
+);
+
+CREATE TABLE IF NOT EXISTS doacoes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    anonymous TINYINT(1) DEFAULT 0,
+    instituicao_id INT NOT NULL,
+    valor DOUBLE NOT NULL,
+    message VARCHAR(255),
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (instituicao_id) REFERENCES instituicoes(id)
+);
+
+-- CREATE TABLE IF NOT EXISTS sessions(
+--     id PRIMARY KEY,
+--     user_id INT NOT NULL,
+--     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     FOREIGN KEY (user_id) REFERENCES users(id)
+-- );
+
+INSERT INTO userType(name)
+VALUES ("usuario"), ("admin");
+
+INSERT INTO accountType(name)
+VALUES ("usuario"), ("instituicao");
+
+INSERT INTO profile(description, picture, email_publico)
+VALUES ("eu sou o admin", "/default.png", "");
+
+INSERT INTO profile(description, picture, email_publico)
+VALUES ("O Instituto Ramacrisna transforma vidas há décadas, oferecendo educação, cultura e oportunidades reais para crianças e jovens em situação de vulnerabilidade", "/ramacrisna.png", "faleconosco@ramacrisna.org.br");
+
+INSERT INTO profile(description, picture, email_publico)
+VALUES ("A Fundação Pão dos Pobres acolhe, forma e prepara adolescentes para o futuro, com afeto e estrutura.", "/pao_dos_pobres.png", "relacaoinstitucional@paodospobres.com.br");
+
+INSERT INTO users(name, tipo, profile_id)
+VALUES ("Joao guilherme", 2, 1);
+
+INSERT INTO account (id, username, email, password, tipo, id_outro)
+VALUES (1, 'Admin_User', 'admin@example.com', "senha", 1, LAST_INSERT_ID());
+
+INSERT INTO instituicoes(name, cnpj, link, profile_id)
+VALUES ("institutio ramacrisna", "000000000000", "https://ramacrisna.org.br/", 2);
+
+INSERT INTO account(username, password, email, tipo, id_outro)
+VALUES ("ramacrisna", "senha", 'faleconosco@ramacrisna.org.br', 2, LAST_INSERT_ID());
+
+INSERT INTO instituicoes(name, cnpj, link, profile_id)
+VALUES ("pao dos pobres", "000000000001","https://www.paodospobres.org.br/", 3);
+
+INSERT INTO account(username, password, email, tipo, id_outro)
+VALUES ("pao_dos_pobres", "senha", "relacaoinstitucional@paodospobres.com.br", 2, LAST_INSERT_ID());
+
+INSERT INTO doacoes(user_id, instituicao_id, valor)
+VALUES (1, 1, 0.75);
+
+INSERT INTO profile(description, picture, email_publico) VALUES("","/profile.png","");
+INSERT INTO users (name, tipo, profile_id) VALUES ('${username}', 1, LAST_INSERT_ID());
+INSERT INTO account (username, email, password, tipo, id_outro ) VALUES (${username}, ${email}, ${password}, 1, LAST_INSERT_ID());
+
